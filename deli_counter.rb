3fde8df156b98eac967db528e9b6e54c29e61548
Dana Pughakoff <danainjax@gmail.com>
2020-08-04 13:53:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,7 @@ def now_serving(line_array)
   if line_array.empty?
     puts "There is nobody waiting to be served!"
   else
-  line_array.shift (line_array)
-  "Currrently serving #{line_array.shift}."
+  puts "Currrently serving #{line_array.shift}."
   end  
  end
  
